58da6b010fdfc162471143b08afedabe87976085
Ole Aass <oaass@php.net>
2015-11-26 22:58:26 +0100

    Added system::errors as default module and controller

--- app/var/config/router.php
+++ app/var/config/router.php
@@ -5,6 +5,9 @@ $di->setShared('router', function () use ($modules) {
     $router = new \Phalcon\Mvc\Router();
     $router->removeExtraSlashes(true);
 
+    $router->setDefaultModule('system');
+    $router->setDefaultController('errors');
+
     // Import module routes
     foreach ($modules as $module) {
         include dirname($module['path']) . '/routes.php';
